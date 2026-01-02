0bb4bf48f8cb7016b8d94ee92ad1c1293019c3b0
Karina <karinahaimov22@gmail.com>
2026-01-02 17:38:16 -0500

    added code

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,31 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const product = action.payload;
+
+        const existingItem = state.items.find((item) => item.name === product.name);
+  
+        if (existingItem) {
+          // if exists, increase quantity
+          existingItem.quantity += 1;
+        } else {
+          // if new, add with quantity = 1
+          state.items.push({
+            ...product,
+            quantity: 1,
+          });
+        }
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter((item) => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
+        const { name, quantity } = action.payload;
 
+        const itemToUpdate = state.items.find((item) => item.name === name);
+        if (itemToUpdate) {
+          itemToUpdate.quantity = quantity;
+        }
     
     },
   },
